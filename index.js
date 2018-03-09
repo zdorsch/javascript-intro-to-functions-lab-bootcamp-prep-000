77a9a5e3be3901b89b6ccc3abf254cabd29bb4fd
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-09 21:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma('I love you, Grandma.') {
   return "I love you, Grandma."
 }
