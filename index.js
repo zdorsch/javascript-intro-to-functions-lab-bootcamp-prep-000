6287862ad158059897e37b30e590d28e26dc9017
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-09 21:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return ("I can\'t hear you!")
-  else if (string.toUpperCase())
-  return ("I love you, too.")
-  else (string == "I love you, Grandma.")
+  else if (string.toUpperCase() === string)
   return ("YES INDEED!")
+  else (string == "I love you, Grandma.")
+  return ("I love you, too.")
 }
