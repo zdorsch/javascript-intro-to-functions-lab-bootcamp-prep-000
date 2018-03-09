8bb4c3a90f9de81ddb3c98bc69e9d52352553329
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-09 21:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ function sayHiToGrandma(string) {
   return ("I can\'t hear you!")
   else if (string.toUpperCase())
   return ("YES INDEED!")
-  else ( string === "I love you, Grandma"
+  else (string === "I love you, Grandma.")
+  return ("I love you, too.")
 }
