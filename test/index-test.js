7c4932009534a8f6903df9cf09cd421424c819ab
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-09 19:15:06 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -35,9 +35,9 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToGrandma("i love you, grandma.")', function() {
+describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('Hello')).toEqual("I can't hear you!")
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
